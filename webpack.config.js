9385b2cb3aa8e6b9b697e79455eaed7d5bcff152
ViyaletaH <drako1224dragon@gmail.com>
2022-10-22 23:44:24 +0200

    init: start store-task

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+            }
+        ]
+    },
+    output: {
+        publicPath: 'public',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    }
+}
