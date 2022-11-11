9bccd619ff73ca89115992b07dec7a3c8e80b67d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-11-11 22:30:20 +0100

    add: search request animation

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,64 @@
 console.log('hi');
 
-// {
-// const yearRange: HTMLElement = document.querySelector('.year-range')!;
+//цифры на год
+const yearRange: HTMLElement = document.querySelector('.year-range')!;
 
-// function rangeSlide(value: string) {
-//    const yNum: HTMLElement = document.querySelector('.y-num')!;
-//    yNum.innerHTML = value;
-
-//    yearRange.addEventListener('mousemove', () => {
+// yearRange.addEventListener('mousemove', () => {
 //     rangeSlide(this.value);
 // });
 
-//     yearRange.addEventListener('onChange', () => {
+//  yearRange.addEventListener('onChange', () => {
 //         rangeSlide(this.value);
-//     })
-// }
-// }
+// });
+
+function rangeSlide(value: string) {
+   const yNum: HTMLElement = document.querySelector('.y-num')!;
+   yNum.innerHTML = value;
+}
+
 // onChange="rangeSlide(this.value)" 
 // onmousemove="rangeSlide(this.value)"
 
+//здесь будут цифры на цену
 {
     const priceRange: HTMLElement = document.querySelector('.price-range')!;
 }
 
+//смена фона на городах (ГОТОВА)
     const wrapper: HTMLElement = document.querySelector('.wrapper')!;
     const bang: HTMLElement = document.querySelector('.bang-choice')!;
     const saigon: HTMLElement = document.querySelector('.saigon-choice')!;
     const sing: HTMLElement = document.querySelector('.sing-choice')!;
 
-    bang.addEventListener("click", () => {
+    bang.addEventListener("click", (): void => {
         wrapper.style.backgroundImage = "url(images/bangkok/wp.jpg)";
     })
 
-    saigon.addEventListener("click", () => {
+    saigon.addEventListener("click", (): void => {
         wrapper.style.backgroundImage = "url(images/hochimin/wp.jpg)";
     })
 
-    sing.addEventListener("click", () => {
+    sing.addEventListener("click", (): void => {
         wrapper.style.backgroundImage = "url(images/singapore/wp.jpg)";
         wrapper.style.backgroundSize = "cover";
     })
+
+    const line: HTMLElement = document.querySelector('.line')!;
+    const search: HTMLElement = document.querySelector('.search')!;
+
+//эффект на линии при enter'е (ГОТОВ)
+let checker: boolean = false;
+
+document.onkeydown = function keySearch(event: KeyboardEvent): void {
+  if (event.code === 'Enter') {
+        checker = true;
+        line.classList.add('active');
+  }
+};
+
+document.onkeyup = function keySearch(): void {
+    if(checker === true){
+        line.classList.remove('active');
+  }
+  checker = false;
+};
